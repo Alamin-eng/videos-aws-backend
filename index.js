526b15397be793c790e3df66faae98a76dc4cfdf
Alamin-eng <alaminruben@gmail.com>
2024-06-20 00:49:40 +0100

    sucurity added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config(); // Notice how we connect env
 const express = require("express");
 const cors = require("cors");
 
@@ -6,12 +6,12 @@ const vidoesStaticData = require("./exampleresponse.json");
 const app = express();
 app.use(cors());
 
-const{ Pool }= require("pg");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: `${process.env.PASSWORD}`,
-  host: "myfirstdb.c3uwgceuc087.eu-north-1.rds.amazonaws.com",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: 5432,
   database: "postgres",
   ssl: {
@@ -22,7 +22,7 @@ const pool = new Pool({
 app.get("/", async (req, res) => {
   // res.json(vidoesStaticData);
   let data = await pool.query("SELECT * FROM videosurl;");
-  res.json(data.rows)
+  res.json(data.rows);
 });
 
 const PORT = 3001;
