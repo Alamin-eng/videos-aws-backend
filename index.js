a8e04109f7fe6b5eb015bd5c0e4db010b35fc647
Alamin-eng <alaminruben@gmail.com>
2024-06-19 23:52:19 +0100

    pool connect

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+
 const express = require("express");
 const cors = require("cors");
 
-const vidoesStaticData = require("./exampleresponse.json")
+const vidoesStaticData = require("./exampleresponse.json");
 const app = express();
 app.use(cors());
 
+const{ Pool }= require("pg");
 
-app.get("/", (req, res) => { 
-  res.json(vidoesStaticData);
+const pool = new Pool({
+  user: "postgres",
+  password: `${process.env.PASSWORD}`,
+  host: "myfirstdb.c3uwgceuc087.eu-north-1.rds.amazonaws.com",
+  port: 5432,
+  database: "postgres",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/", async (req, res) => {
+  // res.json(vidoesStaticData);
+  let data = await pool.query("SELECT * FROM videosurl;");
+  res.json(data.rows)
 });
 
 const PORT = 3001;
